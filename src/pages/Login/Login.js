1e9ae0cfc1ad7a2b9980de61200e7756bc143a55
yoselyng123 <m.yoselyn@correo.unimet.edu.ve>
2024-05-16 16:03:51 -0400

    Update Login.js

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,55 +1,76 @@
 import React from 'react';
 import './Login.css';
 import FacebookIcon from '@material-ui/icons/Facebook';
+import { useHistory } from 'react-router-dom';
 
 function Login() {
-    return (
-        <div className='login'>
-            <div className='login__wrapper'>
-                <div className='login__header'>
-                    <img
-                        className='header__img'
-                        src='https://i.ibb.co/r5krrdz/logo.png'
-                        alt='Netflix Logo'
-                    />
-                </div>
+  const history = useHistory();
 
-                <div className='login__signIn-form'>
-                    <div className='login__signIn-form-top'>
-                        <h1>Sign In</h1>
-                        <input
-                            className='login__input'
-                            type='text'
-                            placeholder='Email or phone number'
-                        />
-                        <input className='login__input' type='text' placeholder='Password' />
-                        <button className='login__btn'>Sign In</button>
-                        <div className='login__signIn-form-top-footer'>
-                            <div className="login__rememberMe">
-                                <input
-                                    type='checkbox'
-                                    name='rememberMe'
-                                    id='rememberMe'
-                                />
-                                <span class="checkmark"></span>
-                                <label htmlFor='rememberMe'>Remember me</label>
-                            </div>
-                            <a href='/'>Need help?</a>
-                        </div>
-                    </div>
-
-                    <div className='login__signIn-form-bottom'>
-                        <div className='login__facebook'> <FacebookIcon/> <span>Login with Facebook</span> </div>
-                        <span>New to Netflix? <a href='/' className='login__signUpLink'>Sign up now.</a> </span> 
-                        <p>
-                            This page is protected by Google reCAPTCHA to ensure
-                            you're not a bot. <a href='/' className='login__learnMore'>Learn more.</a>{' '}
-                        </p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className='login'>
+      <div className='login__wrapper'>
+        <div className='login__header'>
+          <img
+            className='header__img'
+            src='https://i.ibb.co/r5krrdz/logo.png'
+            alt='Netflix Logo'
+          />
         </div>
-    );
+
+        <div className='login__signIn-form'>
+          <div className='login__signIn-form-top'>
+            <h1>Sign In</h1>
+            <input
+              className='login__input'
+              type='text'
+              placeholder='Email or phone number'
+            />
+            <input
+              className='login__input'
+              type='text'
+              placeholder='Password'
+            />
+            <button
+              className='login__btn'
+              onClick={() => history.push('/profiles')}
+            >
+              Sign In
+            </button>
+            <div className='login__signIn-form-top-footer'>
+              <div className='login__rememberMe'>
+                <input type='checkbox' name='rememberMe' id='rememberMe' />
+                <span class='checkmark'></span>
+                <label htmlFor='rememberMe'>Remember me</label>
+              </div>
+              <a href='/'>Need help?</a>
+            </div>
+          </div>
+
+          <div className='login__signIn-form-bottom'>
+            <div
+              className='login__facebook'
+              onClick={() => history.push('/profiles')}
+            >
+              <FacebookIcon /> <span>Login with Facebook</span>{' '}
+            </div>
+            <span>
+              New to Netflix?{' '}
+              <a href='/' className='login__signUpLink'>
+                Sign up now.
+              </a>{' '}
+            </span>
+            <p>
+              This page is protected by Google reCAPTCHA to ensure you're not a
+              bot.{' '}
+              <a href='/' className='login__learnMore'>
+                Learn more.
+              </a>{' '}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
